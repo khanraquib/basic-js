ab7f4ece40be13d8b3a278ea7717f73cb931473a
khanraquib <57582855+khanraquib@users.noreply.github.com>
2020-02-29 23:50:27 +0530

    jS

---
+++ index.js
@@ -0,0 +1,88 @@
+
+
+
+
+
+// varibles and datatypes
+// var a=5;
+// console.log(typeof(a))
+// var b="hello";
+// console.log(typeof(b))
+// var c;
+// console.log(typeof(c))
+// var d=null;
+// console.log(typeof(d))
+// var e=true;
+// console.log(typeof(e))
+
+//  fucction
+// function mul(a,b){
+//      c=a*b;
+//  } mul(2,4)
+//  console.log(c)
+
+// if statement
+// var a=19;
+// if(a>18){
+//     console.log("you can vote")
+// }
+// else{
+//     console.log("you can not vote")
+// }
+
+
+// else ifs statement
+// var a=-10;
+//  if(a<0){
+//     console.log("a is negative")
+//  }
+//  else if(a>0){
+//     console.log("a is positive")
+//  }
+//  else{
+//     console.log("a is zero")
+//  }
+
+// switch statement
+
+// var a=0;
+// switch(a=2){
+//     case 0:
+//         console.log("a is zero")
+//         break;
+//         case 1:
+//             console.log("a is positive")
+//             break;
+//             default:
+//                 console.log("a is negative")
+// }
+
+
+// for loop
+// var a=5;
+// for(i=0;i<a;i++){
+//     console.log(i)
+// }
+
+// while loop
+
+// var a=5;
+// while(a<10){
+
+//     a++;
+//     console.log(a)
+// } 
+
+
+// do while
+// var a=5;
+// do{
+//     // console.log(a)
+//     a++;
+//      console.log(a)
+// }
+// while(a<10)
+// // console.log(a)
+
+
+
